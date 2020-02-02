c5604107482d030760654b1e1cf8b6037ca523d7
temeteke <temeteke@gmail.com>
2020-02-02 13:19:12 +0900

    Change keymap

--- config.py
+++ config.py
@@ -1,62 +1,13 @@
 import sys
 import os
 import datetime
+import itertools
 
 import pyauto
 from keyhac import *
 
 
 def configure(keymap):
-
-    # --------------------------------------------------------------------
-    # keyhacでCapsLockをモディファイアキー定義して無理矢理使う
-    # http://d.hatena.ne.jp/Koonies/20110906/keyhac_capslock_modifier
-
-    ## 関数実行時にモディファイアの状態をリセットするデコレータ
-    def reset_modifier(func):
-        import functools
-
-        @functools.wraps(func)
-        def _reset_modifier(*args, **kw):
-            # モディファイアの状態を無理矢理リセット
-            keymap.modifier = 0
-            # 関数実行
-            return func(*args, **kw)
-        return _reset_modifier
-
-    ## JobQueue/JobItem でサブスレッド処理にするデコレータ
-    def job_queue(func):
-        import functools
-
-        @functools.wraps(func)
-        def _job_queue(*args, **kw):
-
-            num_items = JobQueue.defaultQueue().numItems()
-            if num_items:   # 処理待ちアイテムがある場合は、その数を表示
-                print(u"JobQueue.defaultQueue().numItems() :", num_items)
-
-            def __job_queue_1(job_item):
-                return func(*args, **kw)
-
-            def __job_queue_2(job_item):
-                # print("job_queue : ", func.__name__, args, kw)
-                pass
-
-            job_item = JobItem(__job_queue_1, __job_queue_2)
-            JobQueue.defaultQueue().enqueue(job_item)
-
-        return _job_queue
-
-    ## 一定時間経過後にモディファイアの状態をリセット
-    @job_queue
-    def auto_reset_modifier():
-        import time
-        WAIT_TIME = 0.5  # 秒
-        time.sleep(WAIT_TIME)
-        # モディファイアの状態を無理矢理リセット
-        # if keymap.modifier: print keymap.modifier
-        keymap.modifier = 0
-
     # --------------------------------------------------------------------
     # config.py編集用のテキストエディタの設定
 
@@ -79,8 +30,13 @@ def configure(keymap):
     keymap_global['O-(29)'] = lambda: keymap.wnd.setImeStatus(0)
     keymap_global['O-(28)'] = lambda: keymap.wnd.setImeStatus(1)
 
+    mod_keys = ['Alt-', 'Ctrl-', 'Shift-', 'Win-']
+    mod_keys_combs = []
+    for i in range(len(mod_keys)+1):
+        mod_keys_combs += [''.join(x) for x in itertools.combinations(mod_keys, i)]
+
     # キー変換
-    for mod_key in ['', 'Alt-', 'Ctrl-', 'Shift-', 'Win-']:
+    for mod_key in mod_keys_combs:
 
         # 共通
         for user_mod_key in ['User0-', 'User1-']:
@@ -128,62 +84,38 @@ def configure(keymap):
     keymap_global['User1-O'] = 'Shift-End', 'Delete'
 
 
-
-    # --------------------------------------------------------------------
-    # CapsLock
-
-    # ユーザモディファイアキーの定義：CapsLock(240) --> U3
-    keymap.defineModifier("(240)", "U3")
-
-    # 単体押しは一定時間後にモディファイアの状態をリセット
-    # keymap_global["U1"] = auto_reset_modifier     # これだとキー表記エラー
-    keymap_global["(240)"] = auto_reset_modifier
-
-    for c in [chr(i) for i in range(65, 91)]:
-        keymap_global[ "U3-" + c ] = reset_modifier(keymap.InputKeyCommand("RC-" + c))
-
-    def set_key_for_capslock(key, command, keymaps=keymap_global):
-        keymaps[ "U3-" + key] = reset_modifier(keymap.InputKeyCommand(command))
-        keymaps[ "RC-" + key] = command
-
-    # CapsLockのキーマップ
-    set_key_for_capslock("J", "PageDown")
-    set_key_for_capslock("K", "PageUp")
-
     # --------------------------------------------------------------------
     # アプリケーション別の設定
 
     # Explorer
-    keymap_explorer = keymap.defineWindowKeymap( exe_name="explorer.exe" )
-    set_key_for_capslock("H", "Alt-Left", keymap_explorer)
-    set_key_for_capslock("L", "Alt-Right", keymap_explorer)
-    keymap_explorer[ "U0-I" ] = "F2"
+    keymap_explorer = keymap.defineWindowKeymap(exe_name='explorer.exe')
+    keymap_explorer['U0-I'] = 'F2'
 
     # Firefox
-    keymap_firefox = keymap.defineWindowKeymap( exe_name="firefox.exe" )
-    keymap_firefox[ "U0-Z" ] = "Ctrl-Shift-T"
-    keymap_firefox[ "U0-X" ] = "Ctrl-W"
-    keymap_firefox[ "U0-C" ] = "Ctrl-PageUp"
-    keymap_firefox[ "U0-V" ] = "Ctrl-PageDown"
-    keymap_firefox[ "U0-N" ] = "Ctrl-T"
-    keymap_firefox[ "U0-I" ] = "Ctrl-L"
+    keymap_firefox = keymap.defineWindowKeymap(exe_name='firefox.exe')
+    keymap_firefox['User0-Z'] = 'Ctrl-Shift-T'
+    keymap_firefox['User0-X'] = 'Ctrl-W'
+    keymap_firefox['User0-C'] = 'Ctrl-PageUp'
+    keymap_firefox['User0-V'] = 'Ctrl-PageDown'
+    keymap_firefox['User0-N'] = 'Ctrl-T'
+    keymap_firefox['User0-I'] = 'Ctrl-L'
 
     # Console
-    keymap_console = keymap.defineWindowKeymap( exe_name="Console.exe" )
-    keymap_console[ "U0-V" ] = "Shift-Insert"
+    keymap_console = keymap.defineWindowKeymap(exe_name='Console.exe')
+    keymap_console['Ctrl-V'] = 'Shift-Insert'
+    keymap_console['User0-V'] = 'Shift-Insert'
+    keymap_console['User1-V'] = 'Ctrl-V'
 
     # mintty
-    keymap_mintty = keymap.defineWindowKeymap( exe_name="mintty.exe" )
-    keymap_mintty[ "U0-V" ] = "Shift-Insert"
-
-    # gvim
-    keymap_gvim = keymap.defineWindowKeymap( exe_name="gvim.exe" )
-    keymap_gvim[ "U0-V" ] = "Shift-Insert"
+    keymap_mintty = keymap.defineWindowKeymap(exe_name='mintty.exe')
+    keymap_mintty['Ctrl-V'] = 'Shift-Insert'
+    keymap_mintty['User0-V'] = 'Shift-Insert'
+    keymap_mintty['User1-V'] = 'Ctrl-V'
 
     # VLC
     keymap_vlc = keymap.defineWindowKeymap(exe_name='vlc.exe')
-    keymap_vlc['U0-Z'] = 'Ctrl-J'
-    keymap_vlc['U0-C'] = 'Ctrl-Left'
-    keymap_vlc['U0-V'] = 'Ctrl-Right'
-    keymap_vlc['U0-M'] = 'Alt-Ctrl-Left'
-    keymap_vlc['U0-Comma'] = 'Alt-Ctrl-Right'
+    keymap_vlc['User0-Z'] = 'Ctrl-J'
+    keymap_vlc['User0-C'] = 'Ctrl-Left'
+    keymap_vlc['User0-V'] = 'Ctrl-Right'
+    keymap_vlc['User0-M'] = 'Alt-Ctrl-Left'
+    keymap_vlc['User0-Comma'] = 'Alt-Ctrl-Right'
